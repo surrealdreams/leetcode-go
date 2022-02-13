03281697fff44deb516e51c38dac8c7ce7f24c59
Frank DeRosa <frankderosa@Franks-MacBook-Pro.local>
2022-02-13 14:17:44 -0500

    Update documentation
    Update the comments and remove some deug code so later I'll remember
    clearly exactly why I did all this.

--- parenthesis/main.go
+++ parenthesis/main.go
@@ -6,6 +6,16 @@ import (
 )
 
 func generateParenthesis(n int) []string {
+	// The min number will be have the correct number of bits to correspond with desired parentheses.
+	// For valid parentheses, the first character will always be a "(", which corresponds to 1 with
+	// The binary analogy.  n corresponds to pairs of parens, so it needs to be doubled, and the 1
+	// contributes a bit, so the number of shifts is (n*2)-1.
+
+	// Min could be further optimized so it's first binary value is the smallest number that
+	// represents a valid bit/parenthesis pattern
+
+	// Max can be optimized as well to be the largest possible bit/parenthesis pattern.
+
 	min := 1 << (n*2 - 1)
 	max := min << 1
 	results := make([]string, 0)
@@ -13,12 +23,14 @@ func generateParenthesis(n int) []string {
 	for i := min; i < max; i++ {
 		binaryString := strconv.FormatInt(int64(i), 2)
 		if isValidParens(binaryString) {
+			// It's a valid binary equivalent, convert it to parens and add to the result set
 			results = append(results, binToParen(binaryString))
 		}
 	}
 	return results
 }
 
+// Replace the binary digit representation with parentheses.
 func binToParen(bin string) string {
 	result := ""
 	for i := 0; i < len(bin); i++ {
@@ -37,7 +49,7 @@ func isValidParens(binaryString string) bool {
 	// To be valid parenthesis, each paren must be opened before it's closed.  Calculate a
 	// "score" for the number by adding 1 for each "1" and subtracting 1 for each "0".
 	// The score must always end equal to 0 and must never become negative during the calculation.
-	// This would indicate parens were closed before opening - invalid!
+	// This would indicate parens were closed before opening - no good!
 	for _, b := range binaryString {
 		switch b {
 		case '1':
@@ -45,15 +57,15 @@ func isValidParens(binaryString string) bool {
 		case '0':
 			score--
 		}
+
 		if score < 0 {
+			// A negative score means more parens have been closed than open.  That's not valid, so stop now.
 			return false
 		}
 	}
 
-	// debug
-	// if score == 0 {
-	// 	fmt.Printf("%q\n", binaryString)
-	// }
+	// Now that the test is complete, check the score.  If it isn't zero, there aren't a
+	// valid set of opening and closing parentheses.
 	return score == 0
 }
 
